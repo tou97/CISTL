d28455ecb302264c13a80fe88835d1d86b5a6f0c
Tim Ou <timothyou97@gmail.com>
2025-02-22 11:57:21 -0500

    docker optimization and cleanup

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
     'postcss-preset-mantine': {},
     'postcss-simple-vars': {
       variables: {
